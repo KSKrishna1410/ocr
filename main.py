bd5c328de6fa2a30287727b6747eaf4ea072aa83
nishanth919 <nishanth.padala@gmail.com>
2025-03-11 08:36:04 +0000

    Table detection code fixies

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import argparse
 from convert_pdftoImage import pdf2Image
 from getKeyValueResults import process_invoice
 from generateKey_mapping import generate_key_mapping
-# from identifyTableData import detectImage
-
+from identifyTableData import detectImage
+import pandas as pd
 # File Path
 # input_folder = r"C:\Users\nisha\Documents\ProductDevelopement\OpenSourceModel\PaddleOCR_research\std_code\v3\docs"  # Folder containing PDFs
 # output_folder = r"C:\Users\nisha\Documents\ProductDevelopement\OpenSourceModel\PaddleOCR_research\std_code\v3\docs\images"  # Folder to save PNGs
@@ -12,10 +12,10 @@ from generateKey_mapping import generate_key_mapping
 def main(input_folder, output_folder, docType):
     # Ensure output folder exists
     os.makedirs(output_folder, exist_ok=True)
-
+    layoutOutput = None
     # Convert PDFs to images and get list of image paths
-    keyMappingData = generate_key_mapping(docType);
-    for file in os.listdir(input_folder):
+    # keyMappingData = generate_key_mapping(docType);
+    for index, file in enumerate(os.listdir(input_folder)):
         file_name = os.path.splitext(file)[0]  # Extract filename without extension
         print(f"Filename: {file_name}")  # Print or store it for further use
         image_paths = pdf2Image(input_folder, output_folder,file)
@@ -30,17 +30,28 @@ def main(input_folder, output_folder, docType):
         for image_path in image_paths:
             print(f"\n🔍 Processing Image: {image_path}")
             page_file_name = os.path.splitext(os.path.basename(image_path))[0]
-            # detectImage(image_path,page_file_name)
-            extracted_data = process_invoice(image_path,page_file_name,output_folder,keyMappingData)
-            base_name = os.path.splitext(os.path.basename(image_path))[0]  # Extract filename without extension
-            output_path = os.path.join(output_folder, f"{base_name}.json")  # Save JSON in output folder
+            layoutOutput = detectImage(image_path,page_file_name,output_folder)
+            
+            # extracted_data = process_invoice(image_path,page_file_name,output_folder,keyMappingData)
+            # base_name = os.path.splitext(os.path.basename(image_path))[0]  # Extract filename without extension
+            # output_path = os.path.join(output_folder, f"{base_name}.json")  # Save JSON in output folder
 
-            # Save JSON file
-            with open(output_path, "w", encoding="utf-8") as json_file:
-                json_file.write(extracted_data)
+            # # Save JSON file
+            # with open(output_path, "w", encoding="utf-8") as json_file:
+            #     json_file.write(extracted_data)
 
-            print(f"✅ JSON output saved: {output_path}")
-    return extracted_data
+            # print(f"✅ JSON output saved: {output_path}")
+        # if index == len(os.listdir(input_folder)) - 1:
+    # Convert to DataFrame
+    df = pd.DataFrame(layoutOutput)
+
+    # Save to CSV
+    csv_path = f"{output_folder}tables_detected.csv"
+    df.to_csv(csv_path, index=False)
+
+    print(f"Saved extracted table details to:\nCSV: {csv_path}\nExcel: {excel_path}")
+    return None
+    # return extracted_data
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Extract invoice details from PDFs in a folder.")
@@ -52,5 +63,5 @@ if __name__ == "__main__":
     
     main(args.input_folder, args.output_folder, args.docType)
 
-
+# python main.py "/workspace/nishanth/newprj/paddleOCRKey/docs/" "/workspace/nishanth/newprj/paddleOCRKey/docs/processDocs" invoice
 # python .\main.py "C:\Users\nisha\Documents\ProductDevelopement\OpenSourceModel\PaddleOCR_research\std_code\v3\docs" "C:\Users\nisha\Documents\ProductDevelopement\OpenSourceModel\PaddleOCR_research\std_code\v3\docs\images"
